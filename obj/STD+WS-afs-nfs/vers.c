41878f899aafd300732b73ba7b186e239f6d6619
Jason Stevens <jason.r.stevens@live.com>
2026-06-06 21:32:07 +0100

    minor cleanup

--- obj/STD+WS-afs-nfs/vers.c
+++ obj/STD+WS-afs-nfs/vers.c
@@ -3,7 +3,7 @@ int  version_minor      = 1;
 char version_variant[]  = "X";
 char version_patch[]    = "";
 int  version_edit       = 147;
-char version[] = "Mach 2.6 #5.1(X147): Sat  6 Jun 2026 21:18:04 BST; obj/STD+WS-afs-nfs (Jasons-Mac-mini.local)\n";
+char version[] = "Mach 2.6 #5.1(X147): Sat  6 Jun 2026 21:26:31 BST; obj/STD+WS-afs-nfs (Jasons-Mac-mini.local)\n";
 char cmu_copyright[] = "\
 /*\n\
  * Mach Operating System\n\
